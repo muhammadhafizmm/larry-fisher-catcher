b8343bba16f64c9b7fa0c522ad7645d40ecb404d
“muhammadhafizmm” <m.hafizmaulana777@gmail.com>
2023-05-28 02:21:04 +0700

    fix(netlify-build-error): forget to push komoditas list component
    forget to push komoditas list
    
    fix build error

--- src/pages/KomoditasList/index.jsx
+++ src/pages/KomoditasList/index.jsx
@@ -1,13 +1,81 @@
-import KomoditasCard from "../../common/KomoditasCard";
-import Search from "../../common/Search";
 import "./style.scss";
+import { useEffect, useState } from "react";
+
+import KomoditasCard, {
+  KomoditasCardSkeleton,
+} from "../../common/KomoditasCard";
+import Search from "../../common/Search";
+
+import { ReactComponent as PlusIcon } from "../../assets/svgs/plus.svg";
+import { getProductList } from "../../service";
 
 function KomoditasList() {
+  const [producs, setProducts] = useState([]);
+  const [query, setQuery] = useState({
+    limit: 10,
+    offset: 0,
+  });
+  const [isReachEnd, setIsReachEnd] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  function handleOnScroll() {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight && !loading) {
+      setQuery((prev) => {
+        return {
+          ...prev,
+          offset: prev.offset + prev.limit,
+        };
+      });
+    }
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    async function fetchListData() {
+      try {
+        const response = await getProductList(query);
+        if (!response.length) {
+          setIsReachEnd(true);
+        }
+        setProducts((prevProducts) => [...prevProducts, ...response]);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    if (!isReachEnd) {
+      fetchListData();
+    }
+  }, [query, isReachEnd]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleOnScroll);
+    return () => window.removeEventListener("scroll", handleOnScroll);
+  }, []);
+
   return (
     <div className="komoditas-container">
       <Search />
       <div className="content">
-        <KomoditasCard/>
+        {loading &&
+          !producs.length &&
+          [...Array(5)].map((_, idx) => (
+            <KomoditasCardSkeleton key={`skeleton_${idx}`} />
+          ))}
+        {producs &&
+          producs.map((product) => (
+            <KomoditasCard key={product.uuid} product={product} />
+          ))}
+        {!isReachEnd && <KomoditasCardSkeleton />}
+      </div>
+      <div className="add-komoditas-container">
+        <div className="button-wrapper">
+          <div className="button">
+            <span>Tambah Akomodasi</span>
+            <PlusIcon />
+          </div>
+        </div>
       </div>
     </div>
   );
