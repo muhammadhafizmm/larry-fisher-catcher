b02ec9e6789ddc084694e9eb6cb0c47899ec6362
“muhammadhafizmm” <m.hafizmaulana777@gmail.com>
2023-05-28 02:08:52 +0700

    feat(komoditas-list): implement fetch komoditas data
    implement fetch komoditas data and infinite scroll
    
    komoditas list

--- src/common/KomoditasCard/index.jsx
+++ src/common/KomoditasCard/index.jsx
@@ -1,38 +1,59 @@
 import { ReactComponent as FishLogo } from "../../assets/svgs/fish.svg";
 import { ReactComponent as ScallerLogo } from "../../assets/svgs/scaller.svg";
-import "./style.scss"
+import {
+  formatStringDate,
+  toIDRCurrency,
+  toTitleCase,
+} from "../../utils/string";
+import "./style.scss";
 
-function KomoditasCard() {
+function KomoditasCard({ product }) {
   return (
     <div className="komoditas-card">
       <div className="komoditas-content">
         <div className="komoditas-info">
           <div className="komoditas-title">
-            <span>Bandeng</span>
+            <span>{toTitleCase(product.komoditas)}</span>
             <FishLogo />
           </div>
           <div className="komoditas-additional-info">
-            <span className="additional-info">Aceh</span>
+            <span className="additional-info">
+              {product.area_provinsi
+                ? toTitleCase(product.area_provinsi)
+                : "Tidak Diketahui"}
+            </span>
             <div className="devider"></div>
-            <span className="additional-info">Aceh Kota</span>
+            <span className="additional-info">
+              {product.area_kota
+                ? toTitleCase(product.area_kota)
+                : "Tidak Diketahui"}
+            </span>
           </div>
         </div>
         <div className="created">
-          <span>Dibuat: 22 Mei 2023</span>
+          <span>
+            Dibuat: {formatStringDate(product.tgl_parsed, "DD MMM YYYY")}
+          </span>
         </div>
       </div>
       <div className="komoditas-content">
         <div className="komoditas-size">
           <ScallerLogo />
-          <span>200 kg</span>
+          <span>{product.size} kg</span>
         </div>
         <div className="komoditas-price">
-          <span className="komoditas-number-price">Rp. 200.000,00</span>
+          <span className="komoditas-number-price">
+            {toIDRCurrency(product.price)}
+          </span>
           <span className="komoditas-measurment">/kg</span>
         </div>
       </div>
     </div>
   );
+}
+
+export function KomoditasCardSkeleton() {
+  return <div className="komoditas-skeleton"></div>;
 }
 
 export default KomoditasCard;
--- src/service/index.js
+++ src/service/index.js
@@ -1,11 +1,36 @@
 import { DOCUMENT, STEIN_PATH } from "./constants";
-import * as SteinStore from "stein-js-client"
+import * as SteinStore from "stein-js-client";
 
 const collections = new SteinStore(STEIN_PATH);
 
-export function getList() {
-  collections
-    .read(DOCUMENT.LIST)
+export async function getProductList(query) {
+  return collections
+    .read(DOCUMENT.LIST, query)
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+}
+
+export async function getProductSize() {
+  return collections
+    .read(DOCUMENT.OPTION_SIZE)
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+}
+
+export async function getProductArea() {
+  return collections
+    .read(DOCUMENT.OPTION_AREA)
+    .then((response) => {
+      return response;
+    })
     .catch((error) => {
       console.warn(error);
     });
